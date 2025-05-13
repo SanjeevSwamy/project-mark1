fcf21e19ef0164bbc5e5afa44bad8863c5646ab8
Sanjeev Swamy <96098664+SanjeevSwamy@users.noreply.github.com>
2025-05-13 12:24:52 +0530

    Update ScanUpload.tsx

--- src/pages/ScanUpload.tsx
+++ src/pages/ScanUpload.tsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UploadCloud, X, FilePlus, FileCheck } from 'lucide-react';
 
-const API_URL = "https://5896-2401-4900-22e7-74b4-d02b-66e1-e428-2e8.ngrok-free.app/predict";
+const API_URL = "https://668a-2401-4900-22e7-74b4-d02b-66e1-e428-2e8.ngrok-free.app/predict";
 
 
 const ScanUpload: React.FC = () => {
